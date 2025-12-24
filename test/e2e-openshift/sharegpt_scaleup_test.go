bf5af63dafce9005912bb6ee857e7b8e4e90e53d
Andrew Anderson <andy@clubanderson.com>
2025-12-23 23:10:06 -0500

    test: verify gateway restart fix in e2e workflow

--- test/e2e-openshift/sharegpt_scaleup_test.go
+++ test/e2e-openshift/sharegpt_scaleup_test.go
@@ -1,6 +1,8 @@
 /*
 Copyright 2025.
 
+ShareGPT Scale-Up Test - E2E test for workload-variant-autoscaler
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
